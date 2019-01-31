8bd2abed5e99a2c30eed04e6d29c6586b51b73f8
George Dunk <gdunkgd@gmail.com>
2019-01-31 10:25:15 +0000

    init

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-// use when testing phone gap as will not get fired in browser
+// this is phonegap specific
 document.addEventListener("deviceready", setup, false);
-
+function setup() {
+/
+ navigator.notification.alert(
+ 'You are the winner!', // message
+ alertDismissed, // callback
+ 'Game Over', // title
+ 'Done' // buttonName
+ );
+};
